ea70f003b9b330a46766abd8490df19636926ca3
Michele Vangelista <m.vangelista@gmail.com>
2016-12-18 16:48:44 +0100

    Definition for makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The goal of this couple of functions is to create special matrix objects
+## that allow their "inverse" to be cached.
+
+
+## The makeCacheMatrix() function creates a special type of matrix that is
+## actually a list of functions that allow to interact with the matrix itself.
+## Objects created with this function store in their environments reference
+## to the matrix itself and to the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) {
+		inv <<- inverse
+	}
+	getinverse <- function() inv
+	list(set = set,
+	     get = get,
+	     setinverse = setinverse,
+	     getinverse = getinverse)
 }
 
 
